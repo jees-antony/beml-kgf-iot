c2b2f2fc2796cfa704a53c20cc735da5545954de
jees_antony <jeesantony019@gmail.com>
2024-07-12 20:32:55 +0530

    dtpicker small

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,16 @@
 // src/components/TopBar.js
 import React from 'react';
-import { FaCog, FaBell, FaBars } from 'react-icons/fa';
+import { FaBell, FaBars } from 'react-icons/fa';
 import './TopBar.css';
 
-const TopBar = ({ toggleNav }) => {
+const TopBar = ({ toggleNav, logoSrc }) => {
   return (
     <div className="top-bar">
       <FaBars className="hamburger-menu" onClick={toggleNav} />
+      <div className="logo-container">
+        <img src={logoSrc} alt="Logo" className="logo" />
+        <span className="company-name">BEML LTD Bangalore</span>
+      </div>
       <div className="icons-container">
         <FaBell className="icon" />
         {/* <FaCog className="icon" /> */}
